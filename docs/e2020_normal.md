65570fb1127507c93f23b4bdefb4ab9b5a99fc7c
Hugo Ribeiro <ribeirohugo@outlook.pt>
2021-02-16 14:25:54 +0000

    Exame 2020 época normal, Exercício 1.

---
+++ docs/e2020_normal.md
@@ -0,0 +1,6 @@
+# ESINF 2020 Normal
+
+## Exercício 1
+
+[Ver código Java](../src/e2020_normal/ex1.java)
+
--- src/e2020_normal/ex1.java
+++ src/e2020_normal/ex1.java
@@ -5,10 +5,72 @@
  */
 package e2020_normal;
 
+import java.util.ArrayList;
+import java.util.List;
+import javafx.util.Pair;
+
 /**
  *
  * @author hugo.ribeiro
  */
 public class ex1 {
+    
+    public static<K, E extends Comparable<E>> List<Pair<K,E>> mergeLists(List<Pair<K,E>> A, List<Pair<K,E>> B) {
+        
+        List<Pair<K, E>> result = new ArrayList<>();
+        
+        while(!A.isEmpty() && !B.isEmpty()) {
+            
+            if(A.isEmpty()) {
+                result.add(B.get(0));
+                B.remove(0);
+                
+            } else if(B.isEmpty()) {
+                result.add(A.get(0));
+                A.remove(0);
+                
+            } else {
+                E valueA = A.get(0).getValue();
+                E valueB = B.get(0).getValue();
+                
+                if(valueB.compareTo(valueA) > 0) {
+                    result.add(A.get(0));
+                    A.remove(0);
+                } else {
+                    result.add(B.get(0));
+                    B.remove(0);
+                }
+            }
+        }
+
+        return result;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        List<Pair<String,Integer>> A = new ArrayList<>();
+        A.add(new Pair<>("A",2));
+        A.add(new Pair<>("A",2));
+        A.add(new Pair<>("A",5));
+        
+        List<Pair<String,Integer>> B = new ArrayList<>();
+        B.add(new Pair<>("B",1));
+        B.add(new Pair<>("B",1));
+        B.add(new Pair<>("B",2));
+        B.add(new Pair<>("B",3));
+        B.add(new Pair<>("B",4));
+        B.add(new Pair<>("B",4));
+        B.add(new Pair<>("B",5));
+        
+        System.out.println("List A: "+A);
+        System.out.println("List B: "+B);
+        
+        List<Pair<String,Integer>> result = mergeLists(A, B);
+        
+        System.out.println("Resultado: "+result);
+    }
     
 }
